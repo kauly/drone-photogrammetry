d3b74fd88c271496fe009687a1f8a686b028d343
kauly <kauly@hotmail.com>
2022-11-20 15:38:45 -0300

    first

---
+++ environment.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production" | "test";
+      NEXT_PUBLIC_MAPBOX_TOKEN: string;
+      NEXT_PUBLIC_CESIUM_ION_TOKEN: string;
+      mapboxAccessToken: string;
+    }
+  }
+}
+
+export {};
